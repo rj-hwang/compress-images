7ec0c40c8f44837ee7252c15cf03c0da7f1dd10c
RJ <rongjihuang@gmail.com>
2019-07-24 08:55:55 +0800

    Polishing

--- do-compress.js
+++ do-compress.js
@@ -1,13 +1,14 @@
-const compress_images = require('compress-images');
+const compress_images = require('compress-images')
 const glob = require("glob")
 const PngQuant = require("pngquant")
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 
-const pngQuantArgs = [256, '--quality', '70-100'];
+const ts = () => new Date().toTimeString().substr(0, 8)
+const pngQuantArgs = [256, '--quality', '70-100']
 
-const fromDir = "H:/ip7-bcsystem-backup/bcdata/raw/bcdata/images/201811";
-const toDir = "H:/ip7-bcsystem-backup/bcdata/raw/bcdata-compressed/images/201811";
+const fromDir = "H:/ip7-bcsystem-backup/bcdata/raw/bcdata/images/201811"
+const toDir = "H:/ip7-bcsystem-backup/bcdata/raw/bcdata-compressed/images/201811"
 
 if (!fs.existsSync(toDir)) {
   console.warn("Create Directory %s", toDir);
@@ -15,23 +16,25 @@ if (!fs.existsSync(toDir)) {
 }
 
 fs.readdir(fromDir, (err, files) => {
-  let totalFiles = files.length;
+  let totalFiles = files.length
   let pngs = files.filter(f => f.toLowerCase().endsWith(".png"))
-  let totalPngs = pngs.length;
-  console.log("totalPngs=%s, totalFiles=%s", totalPngs, totalFiles);
+  let totalPngs = pngs.length
+  let startTime = new Date().getTime()
+  console.log("%s totalPngs=%s, totalFiles=%s", ts(), totalPngs, totalFiles)
 
   // sequence
-  let p = Promise.resolve();
+  let p = Promise.resolve()
   let errorFiles = []
   pngs.forEach((file, index) => {
     p = p.then(() => compressPng(file, index, totalPngs))
       .catch(error => {
         errorFiles.push(file);
-        console.log(error);
+        console.info("%s [%s/%s] error='%s'", ts(), index + 1, total, error);
       });
   });
+
   p.then(() => {
-    if (errorFiles.length > 0) console.error("error compress files = %s", errorFiles.join("\r\n"));
+    if (errorFiles.length > 0) console.error("error compress files = %s", errorFiles.join("\r\n"))
   })
 });
 
@@ -39,20 +42,20 @@ function compressPng(file, index, total) {
   return new Promise((resolve, reject) => {
     let target = path.resolve(toDir, file)
     if (fs.existsSync(target)) {
-      console.info("%s [%s/%s] ignore because target file exists: '%s'", new Date().toTimeString().substr(0, 8), index + 1, total, target);
+      console.info("%s [%s/%s] ignore because target file exists: '%s'", new Date().toTimeString().substr(0, 8), index + 1, total, target)
       resolve()
     } else {
       let source = path.resolve(fromDir, file)
-      //console.info("%s [%s/%s] from=%s, to=%s", new Date().toTimeString().substr(0, 8), index + 1, total, source, target);
-      const input = fs.createReadStream(source);
-      const output = fs.createWriteStream(target);
-      output.on('error', reject);
-      input.on('error', reject);
+      //console.info("%s [%s/%s] from=%s, to=%s", new Date().toTimeString().substr(0, 8), index + 1, total, source, target)
+      const input = fs.createReadStream(source)
+      const output = fs.createWriteStream(target)
+      output.on('error', reject)
+      input.on('error', reject)
       output.on('finish', () => {
-        console.info("%s [%s/%s] from='%s', to='%s'", new Date().toTimeString().substr(0, 8), index + 1, total, source, target);
-        resolve();
+        console.info("%s [%s/%s] from='%s', to='%s'", ts(), index + 1, total, source, target)
+        resolve()
       });
-      input.pipe(new PngQuant(pngQuantArgs)).pipe(output);
+      input.pipe(new PngQuant(pngQuantArgs)).pipe(output)
     }
   });
 }
